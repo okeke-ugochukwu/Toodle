1df5e7787e9758a661694dfc60f2841885c10eb6
Ugo <okekeugochukwu.dev@gmail.com>
2022-02-27 08:59:27 +0100

    deploy

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,5 @@ npm run build
 cd dist
 git add -A
 git commit -m 'deploy'
-git push -f https://github.com/okeke-ugochukwu/Toodle.git main:vue-verion-live
+git push -f https://github.com/okeke-ugochukwu/Toodle.git vue-verion-live
 cd ..
